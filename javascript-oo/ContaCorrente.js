1a99ff4703f040d00b400e27cca7e846ba48647e
Stanley Jony <1jony2jony@gmail.com>
2021-06-04 16:28:52 -0300

    05.Construtores e estáticos

--- javascript-oo/ContaCorrente.js
+++ javascript-oo/ContaCorrente.js
@@ -1,6 +1,30 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agencia;
+    static numeroDeContas = 0;
+    _agencia;
+    _cliente;
     _saldo = 0;
+
+    set cliente(cliente){
+        if(cliente instanceof Cliente){
+            this._cliente = cliente;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this._cliente = cliente;
+        this._agencia = agencia;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
