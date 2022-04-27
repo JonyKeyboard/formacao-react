1c340c9f6a089fedf43c1ab7b0b42e0fad7fca96
Stanley <1jony2jony@gmail.com>
2022-04-27 11:05:02 -0300

    Aula 6 e 7 do curso JS

---
+++ basic-JS/aula-7/js/scripts.js
@@ -0,0 +1,24 @@
+var nome = "Jony";
+var nome2 = "Stanley";
+
+var nomeCompleto = nome + " " + nome2;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "nome");
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.23");
+
+var frase = 'Esta é uma frase complexa!';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Uma frase entre "aspas"');
+
+document.write("<br>numero: " + "7");
