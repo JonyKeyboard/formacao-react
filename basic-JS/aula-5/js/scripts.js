e2cba52d316f9c027224317ba8d794cbfdf1ca6a
Stanley <1jony2jony@gmail.com>
2022-04-27 09:03:37 -0300

    5 primeiras aulas do curso JS

---
+++ basic-JS/aula-5/js/scripts.js
@@ -0,0 +1,15 @@
+// var é local
+var teste = 1;
+// aqui é global
+nome = "Mel"
+
+teste = "Jony";
+
+console.log(teste);
+console.log(nome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
