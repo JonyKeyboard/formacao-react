5b0a65cb8fc62d6f91e17bc5248049cb9460a00b
Stanley <1jony2jony@gmail.com>
2022-04-29 09:28:31 -0300

    Renomeando pastas e enviando aulas 19 a 23

---
+++ basic-JS/aula-19/js/scripts.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+while(x < 5){
+    console.log("Testando repetição " + x);
+    x++;
+}
+
+var arr = ['teste1', 'teste2', 'teste3', 'teste4'];
+var y = 0;
+
+while(y <= 3){
+    console.log(arr[y]);
+    y++;
+}
+
+var palavra = "Stanley";
+var i = 0;
+
+while(i < 7){
+    console.log(palavra[i]);
+    i+=1;
+}
---
+++ basic-JS/aula-23/js/scripts.js
@@ -0,0 +1,20 @@
+function primeiraFuncao(){
+    console.log("Hello Mundo das Funções");
+}
+
+primeiraFuncao();
+
+function dizerNome(nome){
+    console.log("The name is: " + nome);
+}
+
+dizerNome("Stanley");
+dizerNome("Mel");
+dizerNome("Theo");
+
+function soma(a, b){
+    var soma = a + b;
+    return soma;
+}
+
+console.log(soma(2,5));
