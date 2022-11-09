58711e6b55db37e3818f8dbc022d721dfee958b4
Stanley <1jony2jony@gmail.com>
2022-11-09 15:50:19 -0300

    Finalizando aulas 13 a 16

--- js-course/js/scripts.js
+++ js-course/js/scripts.js
@@ -24,16 +24,94 @@
 
 // console.log(soma_args(2,3));
 
-function valor_imc(peso,altura) {
-    var imc = peso / (altura * altura);
-    return imc;
-}
+// function valor_imc(peso,altura) {
+//     var imc = peso / (altura * altura);
+//     return imc;
+// }
 
-var meu_peso = parseFloat(document.getElementById("peso").innerHTML);
-var minha_altura = parseFloat(document.getElementById("altura").innerHTML);
+// var meu_peso = parseFloat(document.getElementById("peso").innerHTML);
+// var minha_altura = parseFloat(document.getElementById("altura").innerHTML);
 
-var meu_imc = valor_imc(meu_peso,minha_altura);
+// var meu_imc = valor_imc(meu_peso,minha_altura);
 
-document.getElementById("peso").innerHTML = meu_peso;
-document.getElementById("altura").innerHTML = minha_altura;
-document.getElementById("imc").innerHTML = meu_imc.toFixed(2);
+// document.getElementById("peso").innerHTML = meu_peso;
+// document.getElementById("altura").innerHTML = minha_altura;
+// document.getElementById("imc").innerHTML = meu_imc.toFixed(2);
+
+//Aula 12 - Arrays
+
+// var alunos = ["João", "Maria", "José"];
+// var num_primos = [2, 3, 5, 7, 11, 13];
+
+// console.log(alunos.length);
+// console.log(num_primos[2]);
+// console.log(num_primos[10]);
+
+// var grupos = [ 
+//     [ "João" , "Maria" ],
+//     [ "Pedro" , "Joana", "André", "Júlio" ],
+//     [ "Carolina" , "Helena", "Marcelo" ]
+// ];
+
+// console.log(grupos.length);
+// console.log(grupos[1][1]);
+
+//Aula 13 - Objetos
+
+// var funcionario = {
+//     'nome': 'Pedro Souza Gomes',
+//     'ano_nasc': 1972,
+//     'cpf': '111.111.111.11',
+//     'cargo': 'Analista de Sistemas'
+// };
+
+// console.log(funcionario['nome']);
+// console.log(funcionario.ano_nasc);
+
+// funcionario.cargo = "Gerente de TI";
+// funcionario.cnh = "135123215335";
+
+// console.log(funcionario);
+
+// var cursos = [
+//     {
+//         'titulo': 'Aprenda programação em Python 3 com facilidade do zero',
+//         'avaliacoes': 680,
+//         'alunos': 2300,
+//         'categorias': ['programacao', 'tecnologia']
+//     },
+
+//     {
+//         'titulo': 'Aprenda PHP e faça sites dinâmicos',
+//         'avaliacoes': 180,
+//         'alunos': 350,
+//         'categorias': ['desenvolvimento web', 'programacao']
+//     },
+
+//     {
+//         'titulo': 'Excel do Zero ao Avançado',
+//         'avaliacoes': 420,
+//         'alunos': 1800,
+//         'categorias': ['produtividade', 'gestão']
+//     }
+    
+// ];
+
+// console.log(cursos[1].categorias[0]);
+
+var aluno = {
+    'nome': 'Maria',
+    'sobrenome': 'Pereira',
+    'ano_nasc' : 1993,
+    'nome_completo': function() {
+        var nomeCompleto = this.nome + ' ' + this.sobrenome;
+        return nomeCompleto;
+    },
+    'idade' : function (anoAtual) {
+        var sua_idade =  anoAtual - this.ano_nasc;
+        return sua_idade;
+    }
+};
+
+console.log(aluno.nome_completo());
+console.log(aluno.idade(2022));
