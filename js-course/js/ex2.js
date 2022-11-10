f590a55f12022a5f8a12e0756bf07eb29caf0cc8
Stanley <1jony2jony@gmail.com>
2022-11-10 11:48:14 -0300

    Finalizando exercícios seção 2

--- js-course/js/ex2.js
+++ js-course/js/ex2.js
@@ -7,16 +7,16 @@ document.getElementById("resultado").innerHTML = "<strong>"+(num1+num2)+"</stron
 
 //--------------------------------------------------------------------------------------
 
-var num1 = parseFloat(document.getElementById("caixa_azul").innerHTML);
-
 //F = (9 * C / 5) + 32
 
-function celsiusForFahrenheit(celsius_num) {
+function celsiusToFahrenheit(celsius_num) {
     var fahrenheit = (9 * (celsius_num / 5)) + 32;
     return fahrenheit;
 }
 
-var num2 = celsiusForFahrenheit(num1);
+var num1 = parseFloat(document.getElementById("caixa_azul").innerHTML);
+
+var num2 = celsiusToFahrenheit(num1);
 
 document.getElementById("caixa_amarela").innerHTML = num2.toFixed(2);
 
@@ -28,11 +28,11 @@ var grupos = [
     [ "Carolina" , "Helena", "Marcelo" ]
 ]; 
 
-grupos.push(grupos[1]);
-grupos.push(grupos[2]);
-grupos.push(["Mariana", "Felipe", "Carla"]);
 
-console.log(grupos);
+var novos_grupos = grupos.slice(-2)
+novos_grupos.push(["Mariana", "Felipe", "Carla"]);
+
+console.log(novos_grupos);
 
 //--------------------------------------------------------------------------------------
 
@@ -43,24 +43,44 @@ var curso = {
     'n_aval_4_estrelas': 80,
     'n_aval_3_estrelas': 33,
     'n_aval_2_estrelas': 20,
-    'n_aval_1_estrela': 4
+    'n_aval_1_estrelas': 4,
+    'total_aval' : function () {
+        var tot_aval = this.n_aval_1_estrelas + this.n_aval_2_estrelas + this.n_aval_3_estrelas + this.n_aval_4_estrelas + this.n_aval_5_estrelas;
+        return tot_aval;
+    },
+    'media_aval' : function () {
+        var med_aval = (( 5 * this.n_aval_5_estrelas ) + ( 4 * this.n_aval_4_estrelas ) + ( 3 * this.n_aval_3_estrelas ) + ( 2 * this.n_aval_2_estrelas ) + ( 1 * this.n_aval_1_estrelas ));
+        return med_aval/this.total_aval();
+        //console.log(med_aval);
+    }
 }
 
 document.getElementById("categoria_principal").innerHTML = curso.categoria[0];
 
-function totAv(num1,num2,num3,num4,num5) {
-    total = num1+num2+num3+num4+num5;
-    return total;
+document.getElementById("total_aval").innerHTML = curso.total_aval();
+
+document.getElementById("media_aval").innerHTML = parseFloat(curso.media_aval());
+
+console.log(curso.media_aval());
+
+//--------------------------------------------------------------------------------------
+
+var pessoa = {
+    'nome' : 'Stanley',
+    'sobrenome' : 'Jony',
+    'email' : '1jony2jony@gmailcom'
 }
 
-document.getElementById("total_aval").innerHTML = totAv(curso.n_aval_1_estrela, curso.n_aval_2_estrelas,curso.n_aval_3_estrelas,curso.n_aval_4_estrelas,curso.n_aval_5_estrelas);
+function criar_tabela(person) {
+    var tabelaHTML = '<div class="tableBox">';
+    tabelaHTML +=       '<table>';
+    tabelaHTML +=           '<tr><th>Nome Completo</th><th>Email</th></tr>';
+    tabelaHTML +=           '<tr><td>'+ person.nome + '' + person.sobrenome + '</td><td>'+ person.email +'</td></tr>';
+    tabelaHTML +=       '</table>';
+    tabelaHTML +=    '</div>';
 
-function medPond(num1,num2,num3,num4,num5) {
-    total = num1+num2+num3+num4+num5;
-    mepo = ((1*num1)+(2*num2)+(3*num3)+(4*num4)+(5*num5))/total;
-
-    return mepo;
+    return tabelaHTML;
 }
 
-document.getElementById("media_aval").innerHTML = medPond(curso.n_aval_1_estrela, curso.n_aval_2_estrelas,curso.n_aval_3_estrelas,curso.n_aval_4_estrelas,curso.n_aval_5_estrelas);
+document.getElementById("tabela").innerHTML = criar_tabela(pessoa);
 
