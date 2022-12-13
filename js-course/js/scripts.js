f7d53c7c7265085a18bc3d475b449697f49bbb45
stanley jony <1jony2jony@gmail.com>
2022-12-13 16:30:34 -0300

    secao:5 - aula 37 localStorage

--- js-course/js/scripts.js
+++ js-course/js/scripts.js
@@ -1,8 +1,57 @@
 // Aula 24 - BOM
 
-window.onmousemove = function(e) {
+// window.onmousemove = function(e) {
 
-    if(e.pageY < 5){
-        alert('Não perca os descontos exclusivos na seção de promoções');
-    }
+//     if(e.pageY < 5){
+//         alert('Não perca os descontos exclusivos na seção de promoções');
+//     }
+// }
+
+// Aula 25 - localStorage
+
+// localStorage.setItem("nome","João");
+
+// console.log(localStorage);
+
+// localStorage.removeItem("nome");
+
+// console.log(localStorage);
+
+document.getElementById("enviar-nome").onclick = function (){
+
+    //guardar o nome digitado em localStorage
+    var nome = document.getElementById("nome-usuario").value;
+    localStorage.setItem("nome", nome);
+
+    //esconde o formulario
+    document.getElementById("name-field").style.display = "none";
+
+    //atualizar e mostrar mensagem de boas vindas
+    document.getElementById("welcome-text").innerHTML = "Olá " + localStorage.nome + ", tudo bem?";
+    document.getElementById("not-me").innerHTML = "Não é " + localStorage.nome + "?";
+    document.getElementById("welcome-area").style.display = "initial";
+
+};
+
+if(localStorage.nome) {
+    
+    //esconde o formulario
+    document.getElementById("name-field").style.display = "none";
+
+    //atualizar e mostrar mensagem de boas vindas
+    document.getElementById("welcome-text").innerHTML = "Olá " + localStorage.nome + ", tudo bem?";
+    document.getElementById("not-me").innerHTML = "Não é " + localStorage.nome + "?";
+    document.getElementById("welcome-area").style.display = "initial";
+
 }
+
+document.getElementById("not-me").onclick = function(){
+
+    //remover chave do localStorage
+    localStorage.removeItem("nome");
+    //esconder mensagem de boas vindas
+    document.getElementById("welcome-area").style.display = "none";
+    //mostrar formulário
+    document.getElementById("name-field").style.display = "initial";
+
+};
