2fe493721ae16752ef73b7051a4bc5368347fb9e
Stanley Jony <stanleyjony2@gmail.com>
2022-12-25 21:58:40 -0300

    Finalizando projeto 2 - Orçamento online

--- site_camisas/js/scripts.js
+++ site_camisas/js/scripts.js
@@ -139,6 +139,43 @@ $(function(){
 
     }
 
+    function atualizar_campos(parametros){
+
+        //cor
+        $("#cor").children().removeClass("selected");
+        var id_cor = "#" + parametros.cor;
+        $(id_cor).addClass("selected");
+
+        //gola
+        $("#gola").children().removeClass("selected");
+        var id_gola = "#" + parametros.gola;
+        $(id_gola).addClass("selected");
+
+        //qualidade
+        $("#qualidade").children().removeClass("selected");
+        var id_qualidade = "#" + parametros.qualidade;
+        $(id_qualidade).addClass("selected");
+
+        //estampa
+        $("#estampa").val(parametros.estampa);
+
+        //embalagem
+        $("#embalagem").val(parametros.embalagem);
+
+        //quantidade
+        $("#quantidade").val(parametros.quantidade);
+
+    }
+
+    function atualizar_localStorage(parametros){
+        window.localStorage.setItem("quantidade", parametros.quantidade);
+        window.localStorage.setItem("cor", parametros.cor);
+        window.localStorage.setItem("gola", parametros.gola);
+        window.localStorage.setItem("qualidade", parametros.qualidade);
+        window.localStorage.setItem("estampa", parametros.estampa);
+        window.localStorage.setItem("embalagem", parametros.embalagem);
+    }
+
     $(".option-filter div").click(function(){
         
         $(this).parent().children("div").removeClass("selected");
@@ -146,6 +183,7 @@ $(function(){
 
         var categoria = $(this).parent().attr('id');
         parametros_pesquisa[categoria] = $(this).attr('id');
+        atualizar_localStorage(parametros_pesquisa);
         atualizar_orcamento(parametros_pesquisa);
 
     });
@@ -154,6 +192,7 @@ $(function(){
 
         var parametros_select = $(this).attr('id');
         parametros_pesquisa[parametros_select] = $(this).val();
+        atualizar_localStorage(parametros_pesquisa);
         atualizar_orcamento(parametros_pesquisa);
 
     });
@@ -162,12 +201,38 @@ $(function(){
 
         var parametros_input = $(this).attr('id');
         parametros_pesquisa[parametros_input] = $(this).val();
+        atualizar_localStorage(parametros_pesquisa);
         atualizar_orcamento(parametros_pesquisa);
 
     });
 
-    // ao carregar a página verificar local storage e atualizar a variável parametros_pesquisa
+    // ao carregar a página 
+    
+    if(window.localStorage["quantidade"]){
+        parametros_pesquisa.quantidade = parseInt(window.localStorage["quantidade"]);
+    }
+    
+    if(window.localStorage["cor"]){
+        parametros_pesquisa.cor = window.localStorage["cor"];
+    }
 
+    if(window.localStorage["gola"]){
+        parametros_pesquisa.gola = window.localStorage["gola"];
+    }
+    
+    if(window.localStorage["qualidade"]){
+        parametros_pesquisa.qualidade = window.localStorage["qualidade"];
+    }
+    
+    if(window.localStorage["estampa"]){
+        parametros_pesquisa.estampa = window.localStorage["estampa"];
+    }
+    
+    if(window.localStorage["embalagem"]){
+        parametros_pesquisa.embalagem = window.localStorage["embalagem"];
+    }
+
+    atualizar_campos(parametros_pesquisa);
     atualizar_orcamento(parametros_pesquisa);
 
    
