b4c3c0e7691706c2c7f82af51fce26a2085a9f5c
stanley jony <1jony2jony@gmail.com>
2023-01-05 12:04:39 -0300

    Endentendo ajax com Jquery

--- js-course/js/scripts.js
+++ js-course/js/scripts.js
@@ -354,26 +354,50 @@
 
 // Aula 51 - Ajax - Response
 
+// function mostrar_temperatura(dados) {
+//     var dados_obj = JSON.parse(dados);
+//     console.log('A temperatura em Londres neste momento é de ' + dados_obj.main.temp + " graus Celsius.")
+// }
 
-function mostrar_temperatura(dados) {
-    var dados_obj = JSON.parse(dados);
-    console.log('A temperatura em Londres neste momento é de ' + dados_obj.main.temp + " graus Celsius.")
-}
-
-function tempo_londres(callback) {
-    var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+// function tempo_londres(callback) {
+//     var xhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
                     
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            callback(this.responseText);
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status == 200) {
+//             callback(this.responseText);
             
-        } 
-    };
+//         } 
+//     };
 
-    xhttp.open("GET", "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
-    xhttp.send();
-}
+//     xhttp.open("GET", "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
+//     xhttp.send();
+// }
 
-tempo_londres(mostrar_temperatura);
+// tempo_londres(mostrar_temperatura);
+
+// Aula 52 - Ajax com Jquery
+
+// $.ajax({
+//     url : "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
+//     type: "GET",
+//     dataType: "json",
+//     success: function(data){
+//         console.log(data);
+//     },
+//     error: function(){
+//         console.log("Erro na requisição");
+//     }  
+// });
+// -------------------------------- metodos em cadeia
+$.ajax({
+    url : "https://openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
+    type: "GET",
+    dataType: "json"
+}).done(function(data){
+    console.log(data);
+}).fail(function(){
+    console.log("Erro na requisição");
+});
+
 
 // });
